cfee6ba1ec81fa5c75d58b0f19d752c4640b18b3
ThandokuhleNdlebe <thandotydx@gmail.com>
2024-07-04 20:37:58 +0200

    counter app is launched

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="header">
+      <header className="App-header">
+        <h1>
+          Counter is Zero
+        </h1>
+        
+        
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
