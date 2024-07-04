b9b4e726306c2e1eb0da8d5eaaeebcea6c7b5812
ThandokuhleNdlebe <thandotydx@gmail.com>
2024-07-04 21:53:43 +0200

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {React, useState} from 'react';
+import {React, useState, useEffect} from 'react';
 import './App.css';
 
 
@@ -8,20 +8,20 @@ const App = () => {
   const [count, setCount] = useState(0);
 
   const incrementClick = () => {
-    console.log('clicked');
+    console.log('incrementClick');
     setCount(count + 1);
   };
   
   const decrementClick = () => {
-    console.log('clicked');
+    console.log('decrementClick');
     setCount(count - 1);
   };
 
   return (
     <div >
-       <h1>
-          Counter is Zero
-        </h1>
+      {count > 0 ?
+       <h1>Counter is {count}</h1> : <h1>Counter is Zero</h1> }
+
         <button onClick={incrementClick}>
           Increment
         </button>
